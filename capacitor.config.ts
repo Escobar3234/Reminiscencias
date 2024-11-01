d6d8c700a4dfddeb185ddc99bf690544764b5a9b
Jean <jean_godoyes@fet.edu.co>
2024-11-01 15:55:06 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'remi',
+  webDir: 'www'
+};
+
+export default config;
